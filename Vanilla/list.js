a22a4583ef796f394cdc6ebcc2a7cf556b643315
NathanGloyn <nathans.dropbox@gmail.com>
2014-01-09 22:43:36 +0000

    Display and hide new item controls

--- Vanilla/list.js
+++ Vanilla/list.js
@@ -1,10 +1,45 @@
-function data(){
-  return [{
-    id: 1,
-    name: Apple
-  },{
-    id: 2,
-    name: Orange
-  }]
-  
+function setUp() {
+
+    addHandlers();
+
+    function addHandlers() {
+        console.log("Add handlers");
+        window.document.getElementById('btnNewItem').onclick = displayNewItem;
+        window.document.getElementById('newItem').onkeydown = addNewItemKeyPress;
+        window.document.getElementById('btnAddNewItem').onclick = addNewItem;
+    }
+
+    function displayNewItem() {
+        console.log("New item clicked");
+        window.document.getElementById('newItemDiv').style.visibility = "visible";
+    }
+
+    function addNewItemKeyPress(event) {
+        if (event.keyCode == 13) {
+            addNewItem(event);
+        }
+    }
+
+    function addNewItem(event) {
+        window.document.getElementById('newItemDiv').style.visibility = "hidden";
+    }
+}
+
+
+function List() {
+    this.items = [];
+
+    displayAddItem = function (e) {
+
+    }
+
+    this.addItem = function (text) {
+        items.add(new ListItem(text));
+    }
+
+
+}
+
+function ListItem(text) {
+    this.text = text;
 }
